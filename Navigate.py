60554e6d1dba825e05a76bf111e3baea65e8a94f
harry <harry@mbr-online.co.uk>
2021-01-02 17:01:10 +0000

    Implementing PerfectMemory algorithm

---
+++ Navigate.py
@@ -0,0 +1,37 @@
+import numpy as np
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Navigate:
+
+    def __init__(self, route):
+        self.grid_path = "ant_world_image_databases/grid/"
+        self.grid_data = pd.read_csv("ant_world_image_databases/grid/database_entries.csv", skipinitialspace = True)
+
+        self.route_path = "ant_world_image_databases/routes/"+route+"/"
+        self.route_data = pd.read_csv(self.route_path+"database_entries.csv", skipinitialspace = True)
+
+        self.current = [int(self.route_data['X [mm]'].iloc[0]), int(self.route_data["Y [mm]"].iloc[0]), int(self.route_data["Z [mm]"].iloc[0])]
+        self.goal = [int(self.route_data['X [mm]'].iloc[-1]), int(self.route_data["Y [mm]"].iloc[-1]), int(self.route_data["Z [mm]"].iloc[-1])]
+
+    def run(self, vis_deg, rot_deg):
+
+        print(self.current)
+        view = plt.imread(self.route_path + self.route_data['Filename'][0])
+        #row = self.grid_data.loc[lambda df: df['X [mm]'] == self.current[0] and df['Y [mm]'] == self.current[1] and df['Z [mm]'] == self.current[3]:]
+        #print(self.grid_data["Filename"][self.grid_data["X [mm]"] == self.current[0]])
+        #print(self.grid_data.loc[(self.grid_data['X [mm]'] == self.current[0]) & (self.grid_data['Y [mm]'] == self.current[1])])
+
+        familiarity_dict = {}
+
+        for i in np.arange(0, vis_deg, rot_deg):
+            rotated_view = np.roll(view, i)
+            ssd = np.sum((view-rotated_view)**2)
+            familiarity_dict[i] = ssd
+
+        print(familiarity_dict)
+
+if __name__ == "__main__":
+    nav = Navigate("ant1_route1")
+    nav.run(360, 2)
