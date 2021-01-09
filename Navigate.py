7bc12f8800506077bf602909277596967ed872c5
Cyphio <Harry@mbr-online.co.uk>
2021-01-09 15:01:33 +0000

    Most familiar direction at given points on the grid visualised via arrows using pyplot quiver

--- Navigate.py
+++ Navigate.py
@@ -7,6 +7,8 @@ import cv2
 class Navigate:
 
     def __init__(self, route, vis_deg, rot_deg):
+        self.topdown_view = plt.imread("ant_world_image_databases/topdown_view.png")
+
         self.grid_path = "ant_world_image_databases/grid/"
         self.grid_data = pd.read_csv("ant_world_image_databases/grid/database_entries.csv", skipinitialspace = True)
 
@@ -20,14 +22,22 @@ class Navigate:
         self.rot_deg = rot_deg
 
     def database_analysis(self, x, y):
-        grid_view_familiarity = {}
+        grid_view_familiarity = []
+
         for i in np.arange(0, 100, math.floor(100/x)):
             for j in np.arange(0, 100, math.floor(100/y)):
                 filename = self.grid_data['Filename'].values[(self.grid_data['Grid X'] == i) & (self.grid_data['Grid Y'] == j)][0]
                 grid_view = cv2.imread(self.grid_path + filename)
                 grid_view = cv2.cvtColor(grid_view, cv2.COLOR_BGR2GRAY)
-                grid_view_familiarity[(i, j)] = self.most_familiar_bearing(grid_view)
-        print(grid_view_familiarity)
+                grid_view_familiarity.append(self.most_familiar_bearing(grid_view))
+
+        x_coor, y_coor = np.meshgrid(np.arange(0, 1000, math.floor(1000/x)), np.arange(0, 1000, math.floor(1000/y)))
+        u = [math.cos(n) for n in grid_view_familiarity]
+        v = [math.sin(n) for n in grid_view_familiarity]
+
+        plt.imshow(self.topdown_view)
+        plt.quiver(x_coor, y_coor, u, v)
+        plt.show()
 
     def most_familiar_bearing(self, curr_view):
         route_familiarity = []
@@ -51,4 +61,4 @@ class Navigate:
 
 if __name__ == "__main__":
     nav = Navigate("ant1_route2", 360, 4)
-    nav.database_analysis(3, 3)
+    nav.database_analysis(10, 10)
