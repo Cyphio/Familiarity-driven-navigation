5a32069fca182ad1a40d0d0188b2611fccd7cbd9
Cyphio <Harry@mbr-online.co.uk>
2021-01-16 15:02:56 +0000

    Display of database analysis output complete

--- Navigate.py
+++ Navigate.py
@@ -2,7 +2,6 @@ import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
 import matplotlib.ticker as plticker
 import cv2
 from pyprobar import probar
@@ -22,52 +21,43 @@ class Navigate:
         self.goal = [int(self.route_data['X [mm]'].iloc[-1]), int(self.route_data["Y [mm]"].iloc[-1])]
         self.grid_bounds = [[int((math.floor(((min(self.route[0])-buffer)/10))*10)), int((math.floor(((min(self.route[1])-buffer)/10))*10))],
                             [int((math.ceil(((max(self.route[0])+buffer)/10))*10)), int((math.ceil(((max(self.route[1])+buffer)/10))*10))]]
-        print(self.grid_bounds)
-        print(self.topdown_view.shape)
 
         self.vis_deg = vis_deg
         self.rot_deg = rot_deg
 
     def database_analysis(self, spacing):
-        delta_x = int(self.grid_bounds[1][0]-self.grid_bounds[0][0])
-        delta_y = int(self.grid_bounds[1][1]-self.grid_bounds[0][1])
-
-        print(int(delta_x/spacing))
-
-        print(np.linspace(self.grid_bounds[0][0]/10, self.grid_bounds[1][0]/10, num=int(delta_x/spacing), endpoint=True, dtype=int))
-        print(np.linspace(self.grid_bounds[0][1]/10, self.grid_bounds[1][1]/10, num=int(delta_y/spacing), endpoint=True, dtype=int))
+        x_ticks = np.arange(self.grid_bounds[0][0], self.grid_bounds[1][0]+1, step=spacing, dtype=int)
+        y_ticks = np.arange(self.grid_bounds[0][1], self.grid_bounds[1][1]+1, step=spacing, dtype=int)
 
         grid_view_familiarity = []
-        for i in probar(np.linspace(self.grid_bounds[0][0]/10, self.grid_bounds[1][0]/10, num=int(delta_x/spacing), endpoint=True, dtype=int)):
-            for j in np.linspace(self.grid_bounds[0][1]/10, self.grid_bounds[1][1]/10, num=int(delta_y/spacing), endpoint=True, dtype=int):
-                filename = self.grid_data['Filename'].values[(self.grid_data['Grid X'] == i) & (self.grid_data['Grid Y'] == j)][0]
+        for x in probar(x_ticks):
+            for y in y_ticks:
+                filename = self.grid_data['Filename'].values[(self.grid_data['Grid X'] == x/10) & (self.grid_data['Grid Y'] == y/10)][0]
                 grid_view = cv2.imread(self.grid_path + filename)
                 grid_view = cv2.cvtColor(grid_view, cv2.COLOR_BGR2GRAY)
                 grid_view_familiarity.append(self.most_familiar_bearing(grid_view))
 
         fig = plt.figure(figsize=((self.grid_bounds[1][0]-self.grid_bounds[0][0])/100, (self.grid_bounds[1][1]-self.grid_bounds[0][1])/100))
-        #fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
         ax = fig.add_subplot()
-        loc = plticker.MultipleLocator(base=spacing)
-        ax.xaxis.set_major_locator(loc)
-        ax.yaxis.set_major_locator(loc)
+        ax.xaxis.set_major_locator(plticker.FixedLocator(x_ticks))
+        ax.yaxis.set_major_locator(plticker.FixedLocator(y_ticks))
         ax.grid(which='major', axis='both', linestyle='-')
 
         img = ax.imshow(self.topdown_view)
-        ax.set_xlim([self.grid_bounds[0][0], self.grid_bounds[1][0]])
-        ax.set_ylim([self.grid_bounds[0][1], self.grid_bounds[1][1]])
-
         ax.plot(self.route[0], self.route[1], linewidth=2, color='r')
 
         X, Y = np.meshgrid(
-            np.linspace(self.grid_bounds[0][0], self.grid_bounds[1][0], num=int(delta_x/spacing), endpoint=True, dtype=int),
-            np.linspace(self.grid_bounds[0][1], self.grid_bounds[1][1], num=int(delta_y/spacing), endpoint=True, dtype=int))
+            np.arange(self.grid_bounds[0][0], self.grid_bounds[1][0]+1, step=spacing, dtype=int),
+            np.arange(self.grid_bounds[0][1], self.grid_bounds[1][1]+1, step=spacing, dtype=int))
         u = [math.cos(n) for n in grid_view_familiarity]
         v = [math.sin(n) for n in grid_view_familiarity]
         ax.quiver(X, Y, u, v, color='w', scale_units='xy', scale=(1/spacing)*2, width=0.01, headwidth=5)
 
-        plt.xticks(rotation=90)
+        ax.set_xlim([self.grid_bounds[0][0], self.grid_bounds[1][0]])
+        ax.set_ylim([self.grid_bounds[0][1], self.grid_bounds[1][1]])
+
         plt.show()
 
     def most_familiar_bearing(self, curr_view):
@@ -89,4 +79,4 @@ class Navigate:
 
 if __name__ == "__main__":
     nav = Navigate(route="ant1_route8", vis_deg=360, rot_deg=4, buffer=0)
-    nav.database_analysis(30)
+    nav.database_analysis(40)
