692cb51e269d161fc20746d4848b2a3763e35dbb
Cyphio <Harry@mbr-online.co.uk>
2021-01-17 18:32:30 +0000

    Added ability to set custom bounds, for examining specific parts of a route

--- Navigate.py
+++ Navigate.py
@@ -21,12 +21,15 @@ class Navigate:
         self.start = [int(self.route_data['X [mm]'].iloc[0]/10), int(self.route_data["Y [mm]"].iloc[0]/10)]
         self.goal = [int(self.route_data['X [mm]'].iloc[-1]/10), int(self.route_data["Y [mm]"].iloc[-1]/10)]
         self.bounds = [[int((math.floor((min(self.route[0]) / 10)) * 10)), int((math.floor((min(self.route[1]) / 10)) * 10))],
-                       [int((math.ceil((max(self.route[0]) / 10)) * 10)), int((math.ceil((max(self.route[1]) / 10)) * 10))]]
+                            [int((math.ceil((max(self.route[0]) / 10)) * 10)), int((math.ceil((max(self.route[1]) / 10)) * 10))]]
 
         self.vis_deg = vis_deg
         self.rot_deg = rot_deg
 
-    def database_analysis(self, spacing):
+    def database_analysis(self, spacing, bounds=None):
+        if bounds is not None:
+            self.bounds = bounds
+
         x_ticks = np.arange(self.bounds[0][0], self.bounds[1][0] + 1, step=spacing, dtype=int)
         y_ticks = np.arange(self.bounds[0][1], self.bounds[1][1] + 1, step=spacing, dtype=int)
 
@@ -36,8 +39,7 @@ class Navigate:
                 curr_view_path = self.grid_data['Filename'].values[(self.grid_data['Grid X'] == x/10) & (self.grid_data['Grid Y'] == y/10)][0]
                 grid_view_familiarity.append(self.perfect_memory(self.downsample(cv2.imread(self.grid_path + curr_view_path))))
 
-        fig = plt.figure(figsize=((self.bounds[1][0] - self.bounds[0][0]) / 100, (self.bounds[1][1] - self.bounds[0][1]) / 100))
-        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+        fig = plt.figure(figsize=(len(x_ticks), len(y_ticks)), dpi=spacing*10)
 
         ax = fig.add_subplot()
         ax.xaxis.set_major_locator(plticker.FixedLocator(x_ticks))
@@ -56,12 +58,13 @@ class Navigate:
 
         ax.set_xlim([self.bounds[0][0], self.bounds[1][0]])
         ax.set_ylim([self.bounds[0][1], self.bounds[1][1]])
+        plt.xticks(rotation=90)
 
         plt.show()
 
     def perfect_memory(self, curr_view):
         view_familiarity = defaultdict(list)
-        for filename in self.route_data['Filename'][::100]:
+        for filename in self.route_data['Filename']:
             route_view = self.downsample(cv2.imread(self.route_path + filename))
             for i in np.arange(0, self.vis_deg, step=self.rot_deg, dtype=int):
                 rotated_view = np.roll(curr_view, int(curr_view.shape[1] * (i / 360)), axis=1)
@@ -76,7 +79,5 @@ class Navigate:
 
 if __name__ == "__main__":
     nav = Navigate(route="ant1_route8", vis_deg=360, rot_deg=4)
-    nav.database_analysis(50)
-
-    # data = nav.perfect_memory(nav.downsample(cv2.imread("ant_world_image_databases/routes/ant1_route8/image_00000.png")))
-    # print(data)
+    nav.database_analysis(20, bounds=[[450, 350], [600, 500]])
+    #nav.database_analysis(50)
