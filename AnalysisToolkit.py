dd4f2629af08fbee8b51530178907d4cc32bd3ae
cyphio <Harry@mbr-online.co.uk>
2021-03-14 12:54:36 +0000

    Generated PM DB analysis and boxplot graphs. Regenerated signal strength and rIDF graphs

--- AnalysisToolkit.py
+++ AnalysisToolkit.py
@@ -38,7 +38,7 @@ class AnalysisToolkit:
         return cv2.resize(view, (90, 17))
 
     def image_difference(self, minuend, subtrahend):
-        return (minuend.astype("float") - subtrahend.astype("float")) ** 2
+        return abs(minuend.astype("float") - subtrahend.astype("float"))
 
     def get_real_heading(self, x, y):
         coor = min(zip(self.route_X, self.route_Y), key=lambda route_coor: ((route_coor[0]-x)**2 + (route_coor[1]-y)**2))
@@ -76,10 +76,10 @@ class AnalysisToolkit:
             quiver_coors = [(x, y) for x in x_ticks for y in y_ticks]
 
         cm = plt.get_cmap('YlOrRd')
-        line_map = [cm(1. * i / (len(self.route_X) - 1)) for i in range(len(self.route_X) - 1)]
+        line_map = [cm(1. * i / len(self.route_X)) for i in range(len(self.route_X))]
         quiver_map = []
 
-        grid_view_familiarity = []
+        data = []
         for x, y in probar(quiver_coors):
             view = cv2.imread(self.grid_path + self.grid_filenames.get((x, y)))
 
@@ -87,10 +87,10 @@ class AnalysisToolkit:
             rFF = self.get_rFF(route_rIDF)
             familiar_heading = self.get_most_familiar_heading(rFF)
 
-            grid_view_familiarity.append({"X_COOR": x, "Y_COOR": y, "HEADING": familiar_heading})
-
             matched_route_view_idx = self.get_matched_route_view_idx(route_rIDF)
             quiver_map.append(line_map[matched_route_view_idx])
+
+            data.append({"X_COOR": x, "Y_COOR": y, "HEADING": familiar_heading, "MATCHED_ROUTE_VIEW_IDX": matched_route_view_idx})
 
         fig = plt.figure(figsize=(len(x_ticks), len(y_ticks)), dpi=750)
         ax = fig.add_subplot()
@@ -103,8 +103,8 @@ class AnalysisToolkit:
         ax.add_patch(plt.Circle((self.route_X[-1], self.route_Y[-1]), 5, color='red'))
 
         X, Y = zip(*quiver_coors)
-        u = [np.sin(np.deg2rad(n["HEADING"])) for n in grid_view_familiarity]
-        v = [np.cos(np.deg2rad(n["HEADING"])) for n in grid_view_familiarity]
+        u = [np.sin(np.deg2rad(n["HEADING"])) for n in data]
+        v = [np.cos(np.deg2rad(n["HEADING"])) for n in data]
         ax.quiver(X, Y, u, v, color=quiver_map, scale_units='xy', scale=(1/spacing)*2, width=0.01, headwidth=5)
 
         ax.xaxis.set_major_locator(plticker.FixedLocator(x_ticks))
@@ -118,7 +118,7 @@ class AnalysisToolkit:
         if save_data:
             filename = self.route_name + '_' + str(np.ptp(x_ticks)) + 'x' + str(np.ptp(y_ticks)) + '_' + str(spacing)
             self.save_plot(plt, "DATABASE_ANALYSIS/", filename)
-            self.save_dict_as_CSV(grid_view_familiarity, "DATABASE_ANALYSIS/", filename)
+            self.save_dict_as_CSV(data, "DATABASE_ANALYSIS/", filename)
         plt.show()
 
     def avg_error(self, data_path):
@@ -127,7 +127,7 @@ class AnalysisToolkit:
         for row in data:
             real_heading = self.get_real_heading(int(row['X_COOR']), int(row['Y_COOR']))
             errors.append(abs(real_heading - int(row['HEADING'])))
-        return np.mean(errors)
+        return float(np.mean(errors))
 
     def prcnt_correct(self, data_path, threshold):
         data = csv.DictReader(open(data_path))
@@ -140,12 +140,14 @@ class AnalysisToolkit:
 
     def error_boxplot(self, data_paths, save_data=False):
         heading_errors = []
+        indexes = []
         for data_path in data_paths:
             data = csv.DictReader(open(data_path))
             heading_errors.append([abs(self.get_real_heading(int(row['X_COOR']), int(row['Y_COOR'])) - int(row['HEADING'])) for row in data])
+            indexes.append(f"AVG ERROR: {round(self.avg_error(data_path), 2)}\nCORRECT: {round(self.prcnt_correct(data_path, 20), 2)}%")
         fig = plt.figure(dpi=750)
         ax = fig.add_subplot()
-        df = pd.DataFrame(heading_errors, index=[""]*len(data_paths))
+        df = pd.DataFrame(heading_errors, indexes)
         df.T.boxplot(vert=False, flierprops=dict(markerfacecolor='r', marker='s'))
         plt.title("Boxplot of errors in determined headings")
         plt.xlabel("Absolute heading error in degrees")
@@ -153,16 +155,16 @@ class AnalysisToolkit:
         ax.xaxis.set_major_locator(plticker.MultipleLocator(10))
         ax.xaxis.set_minor_locator(plticker.AutoMinorLocator())
         plt.tight_layout()
+        plt.xlim(-5)
         if save_data:
             filename = self.route_name + '_BOXPLOT'
             self.save_plot(plt, "DATABASE_ANALYSIS/", filename)
         plt.show()
 
-    def view_analysis(self, view_1, view_2, view_1_heading=0, view_2_heading=0, save_data=False):
+    def view_analysis(self, view_1, view_2, view_1_heading=0, save_data=False):
         rIDF = self.get_view_rIDF(view_1, view_2, view_1_heading)
-        rFF = self.get_rFF(rIDF)
+        familiar_heading = self.get_most_familiar_heading(self.get_rFF(rIDF))
 
-        familiar_heading = self.get_most_familiar_heading(rFF)
         rotated_view = np.roll(view_1, int(view_1.shape[1] * ((familiar_heading - view_1_heading) / self.vis_deg)), axis=1)
 
         rotated_view_downsampled = self.downsample(rotated_view)
@@ -177,7 +179,7 @@ class AnalysisToolkit:
         ax[0].set_title(f"Given view at heading: {familiar_heading}, rotated: {familiar_heading - view_1_heading}")
         ax[0].imshow(cv2.cvtColor(rotated_view_downsampled.astype(np.uint8), cv2.COLOR_BGR2RGB))
         ax[1].set_title(f"view_1 at heading: {familiar_heading}, rotated: {familiar_heading - view_1_heading}")
-        ax[1].imshow(cv2.cvtColor(rotated_view_downsampled.astype(np.uint8), cv2.COLOR_BGR2RGB))
+        ax[1].imshow(cv2.cvtColor(view_2_downsampled.astype(np.uint8), cv2.COLOR_BGR2RGB))
         ax[2].set_title("Image difference")
         ax[2].imshow(cv2.cvtColor(image_difference.astype(np.uint8), cv2.COLOR_BGR2RGB))
         if save_data:
@@ -188,9 +190,13 @@ class AnalysisToolkit:
         plt.plot(*zip(*sorted(rIDF.items())))
         plt.title(f"rIDF\n"
                   f"Confidence: {round(self.get_signal_strength(rIDF), 2)}, Minimum: {round(min(rIDF.values()), 2)}")
+        plt.ylabel("MSE in pixel intensities")
         plt.xlabel("Angle")
-        plt.ylabel("MSE of pixel intensities")
-        plt.ylim([500, 1400])
+        plt.xticks(np.arange(0, 361, 15), rotation=90)
+        plt.ylim(500, 1400)
+        plt.xlim(0, 360)
+        plt.grid(which='major', axis='both', linestyle=':')
+        plt.tight_layout()
         if save_data:
             filename = "RIDF"
             self.save_plot(plt, "VIEW_ANALYSIS/", filename)
@@ -204,19 +210,22 @@ class AnalysisToolkit:
         ground_truth_view_filename = self.route_filenames[ground_truth_view_idx]
         ground_truth_view_heading = self.route_headings[ground_truth_view_idx]
         ground_truth_view = cv2.imread(self.route_path + ground_truth_view_filename)
+
+        rIDF = self.get_view_rIDF(view, ground_truth_view, view_heading)
+        familiar_heading = self.get_most_familiar_heading(self.get_rFF(rIDF))
+
+        rotated_view = np.roll(view, int(view.shape[1] * ((familiar_heading - view_heading) / self.vis_deg)), axis=1)
+
+        rotated_view_downsampled = self.downsample(rotated_view)
         ground_truth_view_downsampled = self.downsample(ground_truth_view)
 
-        rotated_view = np.roll(view, int(view.shape[1] * ((ground_truth_view_heading - view_heading) / self.vis_deg)), axis=1)
-        rotated_view_downsampled = self.downsample(rotated_view)
-
         image_difference = self.image_difference(rotated_view_downsampled, ground_truth_view_downsampled)
-        view_rIDF = self.get_view_rIDF(rotated_view, ground_truth_view, ground_truth_view_heading)
 
         plt.figure(dpi=750)
         fig, ax = plt.subplots(3, 1)
         fig.tight_layout(pad=2.0, w_pad=0)
 
-        ax[0].set_title(f"View at: ({view_x}, {view_y}) at heading: {ground_truth_view_heading}, rotated: {ground_truth_view_heading - view_heading}")
+        ax[0].set_title(f"View at: ({view_x}, {view_y}) at familiar heading: {familiar_heading}, ground truth offset: {abs(familiar_heading-ground_truth_view_heading)}")
         ax[0].imshow(cv2.cvtColor(rotated_view_downsampled.astype(np.uint8), cv2.COLOR_BGR2RGB))
         ax[1].set_title(f"Ground truth route view: {ground_truth_view_filename} at heading: {ground_truth_view_heading}")
         ax[1].imshow(cv2.cvtColor(ground_truth_view_downsampled.astype(np.uint8), cv2.COLOR_BGR2RGB))
@@ -227,12 +236,16 @@ class AnalysisToolkit:
             self.save_plot(plt, "VIEW_ANALYSIS/", filename)
         plt.show()
 
-        plt.plot(*zip(*sorted(view_rIDF.items())))
+        plt.plot(*zip(*sorted(rIDF.items())))
         plt.title(f"rIDF between given view and {ground_truth_view_filename}\n"
-                  f"Confidence: {round(self.get_signal_strength(view_rIDF), 2)}, Minimum: {round(min(view_rIDF.values()), 2)}")
-        plt.xlabel("Angle")
+                  f"Confidence: {round(self.get_signal_strength(rIDF), 2)}, Minimum: {round(min(rIDF.values()), 2)}")
         plt.ylabel("MSE in pixel intensities")
+        plt.xlabel("Angle")
+        plt.xticks(np.arange(0, 361, 15), rotation=90)
         plt.ylim(500, 1400)
+        plt.xlim(0, 360)
+        plt.grid(which='major', axis='both', linestyle=':')
+        plt.tight_layout()
         if save_data:
             filename = "RIDF"
             self.save_plot(plt, "VIEW_ANALYSIS/", filename)
@@ -261,7 +274,7 @@ class AnalysisToolkit:
         fig, ax = plt.subplots(3, 1)
         fig.tight_layout(pad=2.0, w_pad=0)
 
-        ax[0].set_title(f"View at: ({view_x}, {view_y}) at heading: {matched_route_view_heading}, rotated: {matched_route_view_heading - view_heading}")
+        ax[0].set_title(f"View at: ({view_x}, {view_y}) at familiar heading: {familiar_heading}, best match offset: {abs(familiar_heading-matched_route_view_heading)}")
         ax[0].imshow(cv2.cvtColor(rotated_view_downsampled.astype(np.uint8), cv2.COLOR_BGR2RGB))
         ax[1].set_title(f"Best matched route view: {matched_route_view_filename} at heading: {matched_route_view_heading}")
         ax[1].imshow(cv2.cvtColor(matched_route_view_downsampled.astype(np.uint8), cv2.COLOR_BGR2RGB))
@@ -275,9 +288,13 @@ class AnalysisToolkit:
         plt.plot(*zip(*sorted(view_rIDF.items())))
         plt.title(f"rIDF between given view and {matched_route_view_filename}\n"
                   f"Confidence: {round(self.get_signal_strength(view_rIDF), 2)}, Minimum: {round(min(view_rIDF.values()), 2)}")
-        plt.xlabel("Angle")
         plt.ylabel("MSE in pixel intensities")
+        plt.xlabel("Angle")
+        plt.xticks(np.arange(0, 361, 15), rotation=90)
         plt.ylim(500, 1400)
+        plt.xlim(0, 360)
+        plt.grid(which='major', axis='both', linestyle=':')
+        plt.tight_layout()
         if save_data:
             filename = "RIDF"
             self.save_plot(plt, "VIEW_ANALYSIS/", filename)
@@ -292,7 +309,7 @@ class AnalysisToolkit:
         ax.imshow(self.topdown_view)
 
         cm = plt.get_cmap('YlOrRd')
-        line_map = [cm(1. * i / (len(self.route_filenames) - 1)) for i in range(len(self.route_filenames) - 1)]
+        line_map = [cm(1. * i / len(self.route_filenames)) for i in range(len(self.route_filenames))]
         ax.set_prop_cycle('color', line_map)
         [ax.plot(self.route_X[i:i + 2], self.route_Y[i:i + 2], linewidth=4) for i in range(len(line_map))]
         ax.add_patch(plt.Circle((self.route_X[0], self.route_Y[0]), 10, color='green'))
@@ -318,53 +335,9 @@ class AnalysisToolkit:
             self.save_plot(plt, "VIEW_ANALYSIS/", filename)
         plt.show()
 
-    # Route analysis was used to prove quiver headings are plotted correctly
-    def route_analysis(self, step):
+    def get_enviro_with_markers(self):
         cm = plt.get_cmap('YlOrRd')
-        line_map = [cm(1. * i / (len(self.route_filenames) - 1)) for i in range(len(self.route_filenames) - 1)]
-        quiver_map = []
-
-        route_view_familiarity = {}
-        for idx, filename in enumerate(self.route_filenames[::step]):
-            print(f"Current view under analysis: {filename}")
-            view = cv2.imread(self.route_path + filename)
-            route_rIDF = self.get_route_rIDF(view, self.route_headings[idx*step])
-            rFF = self.get_rFF(route_rIDF)
-
-            familiar_heading = self.get_most_familiar_heading(rFF)
-            matched_route_view_idx = self.get_matched_route_view_idx(route_rIDF)
-
-            route_view_familiarity[str((self.route_X[idx * step], self.route_Y[idx * step]))] = familiar_heading
-            quiver_map.append(line_map[matched_route_view_idx])
-        fig = plt.figure()
-        ax = fig.add_subplot()
-
-        ax.imshow(self.topdown_view)
-        ax.axis('equal')
-
-        ax.set_prop_cycle('color', line_map)
-        [ax.plot(self.route_X[i:i + 2], self.route_Y[i:i + 2], linewidth=1) for i in range(len(line_map))]
-        ax.add_patch(plt.Circle((self.route_X[0], self.route_Y[0]), 10, color='green'))
-        ax.add_patch(plt.Circle((self.route_X[-1], self.route_Y[-1]), 10, color='red'))
-
-        X = [x for x in self.route_X[::step]]
-        Y = [y for y in self.route_Y[::step]]
-        u = [np.sin(np.deg2rad(n)) for n in route_view_familiarity.values()]
-        v = [np.cos(np.deg2rad(n)) for n in route_view_familiarity.values()]
-
-        ax.quiver(X, Y, u, v, color=quiver_map, scale_units='xy')
-
-        ax.set_xlim([self.bounds[0][0], self.bounds[1][0]])
-        ax.set_ylim([self.bounds[0][1], self.bounds[1][1]])
-
-        filename = "ROUTE"
-        self.save_plot(plt, "VIEW_ANALYSIS/", filename)
-
-        plt.show()
-
-    def fig_generator(self):
-        cm = plt.get_cmap('YlOrRd')
-        line_map = [cm(1. * i / (len(self.route_filenames) - 1)) for i in range(len(self.route_filenames) - 1)]
+        line_map = [cm(1. * i / len(self.route_X)) for i in range(len(self.route_X))]
 
         fig = plt.figure()
         ax = fig.add_subplot()
--- PerfectMemory.py
+++ PerfectMemory.py
@@ -2,6 +2,7 @@ from AnalysisToolkit import AnalysisToolkit
 import numpy as np
 import cv2
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
 class PerfectMemory(AnalysisToolkit):
 
@@ -16,7 +17,7 @@ class PerfectMemory(AnalysisToolkit):
         view_rIDF = {}
         for i in np.arange(0, self.vis_deg, step=self.rot_deg, dtype=int):
             view_1_rotated = np.roll(view_1_downsampled, int(view_1_downsampled.shape[1] * (i / self.vis_deg)), axis=1)
-            mse = np.sum(self.image_difference(view_1_rotated, view_2_downsampled))
+            mse = np.sum(self.image_difference(view_1_rotated, view_2_downsampled)**2)
             mse /= float(view_1_downsampled.shape[0] * view_1_downsampled.shape[1])
             view_rIDF[(i + view_1_heading) % self.vis_deg] = mse
         return view_rIDF
@@ -54,11 +55,12 @@ if __name__ == "__main__":
     # pm.database_analysis(spacing=30, save_data=True)
     # pm.database_analysis(spacing=10, bounds=[[490, 370], [550, 460]], save_data=True)
     # pm.database_analysis(spacing=20, corridor=30, save_data=True)
-    # one_px_data_path = "DATABASE_ANALYSIS/PERFECTMEMORY/1_deg_px_res/9-3-2021_12-13-22_ant1_route1_140x740_20.csv"
-    # two_px_data_path = "DATABASE_ANALYSIS/PERFECTMEMORY/2_deg_px_res/9-3-2021_11-19-55_ant1_route1_140x740_20.csv"
-    # four_px_data_path = "DATABASE_ANALYSIS/PERFECTMEMORY/4_deg_px_res/28-2-2021_15-45-13_ant1_route1_140x740_20.csv"
-    # eight_px_data_path = "DATABASE_ANALYSIS/PERFECTMEMORY/8_deg_px_res/9-3-2021_10-45-18_ant1_route1_140x740_20.csv"
-    # pm.error_boxplot([one_px_data_path, two_px_data_path, four_px_data_path, eight_px_data_path], save_data=True)
+    # one_px_data_path = "DATABASE_ANALYSIS/PERFECTMEMORY/1_deg_px_res/10-3-2021_10-18-41_ant1_route1_140x740_20.csv"
+    # two_px_data_path = "DATABASE_ANALYSIS/PERFECTMEMORY/2_deg_px_res/10-3-2021_0-14-54_ant1_route1_140x740_20.csv"
+    # four_px_data_path = "DATABASE_ANALYSIS/PERFECTMEMORY/4_deg_px_res/9-3-2021_22-45-20_ant1_route1_140x740_20.csv"
+    # eight_px_data_path = "DATABASE_ANALYSIS/PERFECTMEMORY/8_deg_px_res/9-3-2021_22-26-6_ant1_route1_140x740_20.csv"
+    # sixteen_px_data_path = "DATABASE_ANALYSIS/PERFECTMEMORY/16_deg_px_res/9-3-2021_22-0-47_ant1_route1_140x740_20.csv"
+    # pm.error_boxplot([one_px_data_path, two_px_data_path, four_px_data_path, eight_px_data_path, sixteen_px_data_path], save_data=False)
     # threshold = 20
     # print(f"AVG error: {pm.avg_error(data_path)}\n% correct: {pm.prcnt_correct(data_path, threshold)}")
 
@@ -66,7 +68,7 @@ if __name__ == "__main__":
     # pm.route_analysis(step=100)
 
     # Off-route view analysis
-    # filename = pm.grid_filenames.get((610, 810))
+    # filename = pm.grid_filenames.get((500, 500))
     # grid_view = cv2.imread(pm.grid_path + filename)
     # pm.view_analysis(view_1=grid_view, view_2=grid_view, save_data=False)
 
@@ -75,10 +77,79 @@ if __name__ == "__main__":
     # filename = pm.route_filenames[idx]
     # route_view = cv2.imread(pm.route_path + filename)
     # route_heading = pm.route_headings[idx]
-    # pm.view_analysis(view_1=grid_view, view_2=route_view, view_2_heading=route_view_heading, save_data=False)
+    # pm.view_analysis(view_1=route_view, view_2=route_view, view_1_heading=route_heading, save_data=False)
 
     # Off-route best matched view analysis
     # pm.best_matched_view_analysis(view_x=610, view_y=810, save_data=True)
 
     # Off-route real match view analysis
-    # pm.ground_truth_view_analysis(view_x=630, view_y=650, save_data=False)
+    # pm.ground_truth_view_analysis(view_x=610, view_y=810, save_data=True)
+
+    # 610.001, 600.01
+    # yellow = cv2.imread(pm.route_path + pm.route_filenames[263])
+    # yellow_heading = pm.route_headings[263]
+    # rIDF = pm.get_view_rIDF(yellow, yellow, yellow_heading)
+    # plt.plot(*zip(*sorted(rIDF.items())))
+    # plt.title(f"rIDF between view at yellow star and itself\n"
+    #           f"Confidence: {round(pm.get_signal_strength(rIDF), 2)}, Minimum: {round(min(rIDF.values()), 2)}")
+    # plt.ylabel("MSE in pixel intensities")
+    # plt.xlabel("Angle")
+    # plt.xticks(np.arange(0, 361, 15), rotation=90)
+    # plt.ylim(500, 1100)
+    # plt.xlim(0, 360)
+    # plt.grid(which='major', axis='both', linestyle=':')
+    # plt.tight_layout()
+    # filename = "YELLOW_RIDF"
+    # pm.save_plot(plt, "MISC/", filename)
+    # plt.show()
+    #
+    # pink = cv2.imread(pm.grid_path + pm.grid_filenames.get((620, 600)))
+    # rIDF = pm.get_view_rIDF(pink, yellow)
+    # plt.plot(*zip(*sorted(rIDF.items())))
+    # plt.title(f"rIDF between view at pink star and view at yellow star\n"
+    #           f"Confidence: {round(pm.get_signal_strength(rIDF), 2)}, Minimum: {round(min(rIDF.values()), 2)}")
+    # plt.ylabel("MSE in pixel intensities")
+    # plt.xlabel("Angle")
+    # plt.xticks(np.arange(0, 361, 15), rotation=90)
+    # plt.ylim(500, 1100)
+    # plt.xlim(0, 360)
+    # plt.grid(which='major', axis='both', linestyle=':')
+    # plt.tight_layout()
+    # filename = "PINK_RIDF"
+    # pm.save_plot(plt, "MISC/", filename)
+    # plt.show()
+    #
+    # green = cv2.imread(pm.grid_path + pm.grid_filenames.get((700, 600)))
+    # rIDF = pm.get_view_rIDF(green, yellow)
+    # plt.plot(*zip(*sorted(rIDF.items())))
+    # plt.title(f"rIDF between view at green star and view at yellow star\n"
+    #           f"Confidence: {round(pm.get_signal_strength(rIDF), 2)}, Minimum: {round(min(rIDF.values()), 2)}")
+    # plt.ylabel("MSE in pixel intensities")
+    # plt.xlabel("Angle")
+    # plt.xticks(np.arange(0, 361, 15), rotation=90)
+    # plt.ylim(500, 1100)
+    # plt.xlim(0, 360)
+    # plt.grid(which='major', axis='both', linestyle=':')
+    # plt.tight_layout()
+    # filename = "GREEN_RIDF"
+    # pm.save_plot(plt, "MISC/", filename)
+    # plt.show()
+    #
+    # on_route = cv2.imread(pm.route_path + pm.route_filenames[263])
+    # data = {}
+    # for x in np.arange(620, 900, step=10, dtype=int):
+    #     off_route = cv2.imread(pm.grid_path + pm.grid_filenames.get((x, 600)))
+    #     data[x-610] = pm.get_signal_strength(pm.get_view_rIDF(off_route, on_route))
+    #
+    # plt.plot(*zip(*sorted(data.items(), reverse=True)))
+    # plt.title("Signal strength over distance")
+    # plt.xlabel("X-axis displacement from yellow star (cm)")
+    # plt.ylabel("Signal strength")
+    # plt.xticks(np.arange(10, 281, 10), rotation=90)
+    # plt.grid(which='major', axis='both', linestyle=':')
+    # plt.tight_layout()
+    #
+    # filename = "GRAPH"
+    # pm.save_plot(plt, "MISC/", filename)
+    #
+    # plt.show()
