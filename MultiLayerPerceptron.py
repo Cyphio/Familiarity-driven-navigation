02747c14ac3c592b695b04cacc6dcaa6c9fe86b4
cyphio <Harry@mbr-online.co.uk>
2021-03-23 00:58:28 +0000

    Final commit tonight. Good night!

--- MultiLayerPerceptron.py
+++ MultiLayerPerceptron.py
@@ -10,6 +10,7 @@ from torch.utils import data
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
+from torch.utils.data import Subset
 from sklearn.model_selection import train_test_split
 
 class MultiLayerPerceptron(AnalysisToolkit, nn.Module):
@@ -19,20 +20,22 @@ class MultiLayerPerceptron(AnalysisToolkit, nn.Module):
         nn.Module.__init__(self)
         self.model_name = 'MLP'
 
-        # self.X_train, self.X_test, self.y_train, self.y_test = self.data_loader(angle=60, test_size=0.33)
-        # print(f"TRAIN X: {type(self.X_train)}")
-        # print(f"TRAIN Y: {type(self.y_train)}")
-
         # MLP hyper-parameters
-        self.INPUT_SIZE = 360
-        self.HIDDEN_SIZE = 360
+        self.INPUT_SIZE = 45
+        self.HIDDEN_SIZE = 1536
+        self.TEST_SIZE = 0.33
         self.EPOCHS = 50
         self.BATCH_SIZE = 64
         self.LEARNING_RATE = 0.001
         self.MOMENTUM = 0.9
 
+        # Data generation
         # self.gen_data(angle=60)
-        print(self.data_loader("ANN_DATA/60"))
+
+        # Data loading
+        dataloaders = self.get_dataloaders("ANN_DATA/60")
+        self.X_train, self.y_train = next(iter(dataloaders['TRAIN']))
+        self.X_test, self.y_test = next(iter(dataloaders['TEST']))
 
         # MLP layers
         self.fc_input = nn.Linear(self.INPUT_SIZE, self.HIDDEN_SIZE)
@@ -60,34 +63,13 @@ class MultiLayerPerceptron(AnalysisToolkit, nn.Module):
         for filename, view, label in df.values:
             plt.imsave(f"./ANN_DATA/{angle}/{label}/{filename}.png", cv2.cvtColor(view.astype(np.uint8), cv2.COLOR_BGR2RGB))
 
-    def data_loader(self, path):
+    def get_dataloaders(self, path):
         transform = transforms.Compose([transforms.ToTensor()])
         dataset = datasets.ImageFolder(path, transform=transform)
-        return torch.utils.data.DataLoader(dataset, batch_size=self.BATCH_SIZE, shuffle=True)
-
-    # def data_loader(self, angle, test_size):
-    #     print('Generating training data...')
-    #     dp = []
-    #     for filename in probar(self.route_filenames):
-    #         view = self.preprocess(cv2.imread(self.route_path + filename))
-    #         # np.append(dp, [torch.from_numpy(view), 1])
-    #         # np.append(dp, [torch.from_numpy(self.rotate(view, angle)), 0])
-    #         # np.append(dp, [torch.from_numpy(self.rotate(view, -angle)), 0])
-    #         dp.append([view, 1])
-    #         dp.append([self.rotate(view, angle), 0])
-    #         dp.append([self.rotate(view, -angle), 0])
-    #     df = pd.DataFrame(dp, columns=['VIEW', 'LABEL'])
-    #     # print(df)
-    #     # sns.countplot(x='LABEL', data=df)
-    #     # plt.show()
-    #     # return train_test_split(df[:, 0:-1], df[:, -1], test_size=test_size, random_state=
-    #     print()
-    #     # print(torch.from_numpy(df['VIEW'].values.astype(np.float32)))
-    #     X = [torch.from_numpy(view) for view in df['VIEW'].values]
-    #     print(X[0])
-    #     y = [torch.tensor(label) for label in df['LABEL'].values]
-    #     return train_test_split(X, y, test_size=test_size, random_state=0, shuffle=True)
-    #     # return train_test_split(torch.Tensor(views), torch.Tensor(labels), test_size=test_size, random_state=0)
+        train_idx, test_idx = train_test_split(list(range(len(dataset))), test_size=self.TEST_SIZE, random_state=0)
+        dataset_dict = {"TRAIN": Subset(dataset, train_idx), "TEST": Subset(dataset, test_idx)}
+        return {tag: torch.utils.data.DataLoader(dataset_dict[tag], batch_size=self.BATCH_SIZE, shuffle=True)
+                for tag in ["TRAIN", "TEST"]}
 
     def forward(self, input):
         fc_input = self.fc_input(input)
@@ -119,4 +101,4 @@ class MultiLayerPerceptron(AnalysisToolkit, nn.Module):
 
 if __name__ == '__main__':
     mlp = MultiLayerPerceptron(route="ant1_route1", vis_deg=360, rot_deg=2)
-    # mlp.train_model()
+    mlp.train_model()
